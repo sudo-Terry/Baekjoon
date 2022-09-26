a1c621af0841a5e716264ec423ce692d39b5d92d
sudo-Terry <76080411+sudo-Terry@users.noreply.github.com>
2022-09-26 23:34:08 +0900

    ADD - 백준 답안지 제출용 defaultForm을 정의하자

---
+++ Baekjoon/_defaultForm.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#define MAX 100100
+using namespace std;
+
+int n, tc;
+
+void solve() {
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cout.tie(0);
+	cin.tie(0);
+	cin >> n;
+  
+  solve();
+
+  return 0;
+}
