a3b0e3e0bda7f64869bc57af97634800b03ea51b
Kihoon Kim <kihoon.kim@cre.ma>
2022-11-02 19:55:16 +0900

    SOLVE - 백준 15650 풀이

--- Baekjoon/pr15650.cpp
+++ Baekjoon/pr15650.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#define MAX 9
 using namespace std;
 
-int n, m;
+int n, m, tc;
+bool visited[MAX + 1] = {
+    0,
+};
+int arr[MAX + 1] = {
+    0,
+};
 
-int main() {
-	cin >> n >> m;
+void dfs(int nodenum, int count)
+{
+  if (count == m)
+  {
+    for (int i = 0; i < m; i++)
+    {
+      cout << arr[i] << ' ';
+    }
+    cout << '\n';
+    return;
+  }
+  for (int i = nodenum; i <= n; i++)
+    if (!visited[i])
+    {
+      visited[i] = true;
+      arr[count] = i;
+      dfs(i + 1, count + 1);
+      visited[i] = false;
+    }
+}
+
+void solve()
+{
+  dfs(1, 0);
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cout.tie(0);
+  cin.tie(0);
+
+  cin >> n >> m;
+
+  solve();
+
+  return 0;
 }
