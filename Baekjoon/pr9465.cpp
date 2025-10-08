6e54a99de000bc2c2972aca3efc7d67dcfbd5969
sudo-Terry <contact.terrykim@gmail.com>
2025-10-08 17:31:09 +0900

    solve: bj9465

--- Baekjoon/pr9465.cpp
+++ Baekjoon/pr9465.cpp
@@ -1,11 +1,54 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <cstring>
 using namespace std;
 
-void solve() {
+#define MAX 100010
 
+int tc, n;
+int sticker[MAX][2];
+int dp[MAX][3];
+
+/*
+dp[i][j] = i 번째에서 j열의 스티커를 떼었을 경우 가중치 합 최댓값
+dp[i+1][0] = max(dp[i][1], dp[i][2])
+dp[i+1][1] = max(dp[i][0], dp[i][2]) + sticker[i+1][0]
+dp[i+1][2] = max(dp[i][0], dp[i][1]) + sticker[i+1][1]
+*/
+
+void solve() {
+  dp[1][0] = 0;
+  dp[1][1] = sticker[0][1];
+  dp[1][2] = sticker[1][1];
+
+  for(int i = 1; i < n; i++){
+    dp[i+1][0] = max(dp[i][1], dp[i][2]);
+    dp[i+1][1] = max(dp[i][0], dp[i][2]) + sticker[0][i+1];
+    dp[i+1][2] = max(dp[i][0], dp[i][1]) + sticker[1][i+1];
+  }
 }
 
 int main() {
+  ios_base::sync_with_stdio(0);
+  cout.tie(0);
+  cin.tie(0);
 
+  cin >> tc;
+  while (tc--){
+
+    memset(dp, -1, sizeof(dp));
+
+    cin >> n;
+    for(int i = 0; i < 2 ; i++){
+      for(int j = 1; j <= n; j++){
+        cin >> sticker[j][i];
+      }
+    }
+    solve();
+
+    cout << max(dp[n][0], max(dp[n][1], dp[n][2])) << '\n';
+  }
+
+  return 0;
 }
