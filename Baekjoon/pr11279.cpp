9fff087b03da4374a12b815cf6f7126b314acc2d
sudo-Terry <contact.terrykim@gmail.com>
2025-10-24 23:20:10 +0900

    solve: bj11279

---
+++ Baekjoon/pr11279.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int N, event;
+priority_queue<int> pq;
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cout.tie(0);
+  cin.tie(0);
+
+  cin >> N;
+
+  while(N--){
+    cin >> event;
+    if(event == 0){
+      if(pq.empty()) cout << '0' << '\n';
+      else {
+        cout << pq.top() << '\n';
+        pq.pop();
+      }
+    }else {
+      pq.push(event);
+    }
+    
+  }
+
+  return 0;
+}
