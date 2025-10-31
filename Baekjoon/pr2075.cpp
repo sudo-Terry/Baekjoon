c25bfe7e4b5799dfb99c4bfb330279ce2c4e74a0
sudo-Terry <contact.terrykim@gmail.com>
2025-11-01 03:47:07 +0900

    solve: bj2075

---
+++ Baekjoon/pr2075.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int N;
+priority_queue<int> pq;
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cout.tie(0);
+  cin.tie(0);
+
+  cin >> N;
+  for(int i = 0; i < N*N; i++){
+    int temp;
+    cin >> temp;
+    pq.push(-1 * temp);
+    if(pq.size() > N) pq.pop();
+  }
+
+  for(int i = 0; i < N - 1; i++) pq.pop();
+
+  cout << -1 * pq.top();
+
+  return 0;
+}
