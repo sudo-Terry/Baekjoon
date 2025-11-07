4a19ce97896e2b1809adf3ac44e0e53b197b18df
sudo-Terry <contact.terrykim@gmail.com>
2025-11-07 22:26:05 +0900

    solve: bj2573

--- Baekjoon/pr2573.cpp
+++ Baekjoon/pr2573.cpp
@@ -4,138 +4,88 @@
 #include <queue>
 #include <vector>
 #include <algorithm>
-#define MAX 10010
+#define MAX 310
+#define ICEBERG_MAX 10000
 using namespace std;
 
 int n, m;
 int ans = 0;
 queue<pair<int, int>> q;
-int dx[] = {0, 1, 0, -1};
-int dy[] = {-1, 0, 1, 0}; // middle up right down left
-bool visited[MAX][MAX] = {
-    0,
-};
-int arr[MAX][MAX] = {
-    0,
-};
-int delta[MAX][MAX] = {
-    0,
-};
+int dx[] = {0, 0, -1, 1}; // 상 하 좌 우
+int dy[] = {1, -1, 0, 0}; 
+bool visited[MAX][MAX];
+int arr[MAX][MAX];
+int delta[MAX][MAX];
+vector<pair<int, int>> iceberg;
 
-bool is_seperated()
-{
-  int temp_x, temp_y;
-  memset(visited, false, sizeof(visited));
-
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < m; j++)
-    {
-      if (arr[i][j] > 0)
-      {
-        temp_y = i;
-        temp_x = j;
-        break;
-      }
-    }
-  }
-
-  q.push(make_pair(temp_y, temp_x));
-  visited[temp_y][temp_x] = true;
-  while (!q.empty())
-  {
-    int y = q.front().first;
-    int x = q.front().second;
-    q.pop();
-    for (int i = 0; i < 4; i++)
-    {
-      int ny = y + dy[i];
-      int nx = x + dx[i];
-      if (arr[ny][nx] > 0 && !visited[ny][nx] && ny < n && nx < m && nx >= 0 && ny >= 0)
-      {
-        q.push(make_pair(ny, nx));
-        visited[ny][nx] = true;
-      }
-    }
-  }
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < m; j++)
-    {
-      if (!visited[i][j] && arr[i][j] > 0)
-        return true;
-    }
+bool is_seperated(){
+  for(pair<int, int> point : iceberg){
+    if (!visited[point.second][point.first] && arr[point.second][point.first] > 0)
+      return true;
   }
   return false;
 }
 
-void bfs()
-{
-  q.push(make_pair(1, 1));
-  visited[1][1] = true;
-  while (!q.empty())
-  {
-    int count_water = 0;
-    int y = q.front().first;
-    int x = q.front().second;
+void bfs(){
+  vector<pair<int,int>> next_iceberg;
+  q.push(make_pair(iceberg[0].first, iceberg[0].second));
+  visited[iceberg[0].second][iceberg[0].first] = true;
+  memset(delta, 0, sizeof(delta));
+  
+  while (!q.empty()){
+    int x = q.front().first;
+    int y = q.front().second;
     q.pop();
-    for (int i = 0; i < 4; i++)
-    {
+    
+    for (int i = 0; i < 4; i++){
       int ny = y + dy[i];
       int nx = x + dx[i];
-      if (arr[ny][nx] <= 0)
-      {
-        count_water++;
-      }
-      if (!visited[ny][nx] && ny < n && nx < m && nx >= 0 && ny >= 0)
-      {
-        q.push(make_pair(ny, nx));
+
+      if(ny >= n || nx >= m || nx < 0 || ny < 0) continue;
+      
+      if (arr[ny][nx] <= 0) delta[y][x]++;
+      
+      if (arr[ny][nx] > 0 && !visited[ny][nx]){
+        q.push(make_pair(nx, ny));
         visited[ny][nx] = true;
       }
     }
-    delta[y][x] = count_water;
   }
 
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < m; j++)
-    {
-      arr[i][j] -= delta[i][j];
-    }
+  for(pair<int, int> point : iceberg){
+    arr[point.second][point.first] -= delta[point.second][point.first];
+
+    if (arr[point.second][point.first] > 0) next_iceberg.push_back(point);
   }
+
+  iceberg.swap(next_iceberg);
 }
 
-void solve()
-{
-  while (true)
-  {
-    bfs();
-    ans++;
-    if (is_seperated())
-    {
-      break;
-    }
-    memset(visited, false, sizeof(visited));
-  }
-  cout << ans;
-}
-
-int main()
-{
+int main(){
   ios_base::sync_with_stdio(0);
   cout.tie(0);
   cin.tie(0);
   cin >> n >> m;
 
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < m; j++)
-    {
+  for (int i = 0; i < n; i++){
+    for (int j = 0; j < m; j++){
       cin >> arr[i][j];
+      if (arr[i][j] > 0) iceberg.push_back({j,i});
     }
   }
 
-  solve();
+  while (true){
+    memset(visited, false, sizeof(visited));
+    if (iceberg.empty()) {
+      cout << 0;
+      return 0;
+    }
 
-  return 0;
+    bfs();
+    if (is_seperated()){
+      cout << ans;
+      return 0;
+    }
+    ans++;
+  }
 }
