ab32994f932c16988db3bc2f004063fe4397329f
sudo-Terry <contact.terrykim@gmail.com>
2025-11-21 18:59:38 +0900

    solve: bj2164

---
+++ Baekjoon/pr2164.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int N;
+  queue<int> q;
+  cin >> N;
+  for(int i = 1; i <= N; i++ ){
+    q.push(i);
+  }
+
+  while(q.size() > 1){
+    int drop = q.front();
+    q.pop();
+
+    int no_drop = q.front();
+    q.pop();
+    q.push(no_drop);
+  }
+
+  q.pop();
+  int last = q.front();
+  cout << last;
+
+  return 0;
+}
