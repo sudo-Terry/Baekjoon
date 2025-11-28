e23087d8ebe7d4a4c4e305bd60484b5a896911b0
sudo-Terry <contact.terrykim@gmail.com>
2025-11-29 05:06:59 +0900

    solve: bj1806

--- Baekjoon/pr1806.cpp
+++ Baekjoon/pr1806.cpp
@@ -12,30 +12,43 @@ int main(){
   vector<int> arr;
 
   cin >> N >> S;
+  int arr_sum = 0;
   for(int i = 0; i < N; i++){
     int temp;
     cin >> temp;
     arr.push_back(temp);
+    arr_sum += temp;
+  }
+
+  if(arr_sum < S){
+    cout << 0;
+    return 0;
   }
 
   int left = 0;
   int right = 0;
   int ans = N; 
+  int sum = arr[0];
   while(true){
-    int sum = 0;
-    for(int i = left; i <= right; i++){
-      sum += arr[i];
+    if (left > right && left < N) {
+      right = left;
+      sum = arr[left];
     }
 
     if(sum >= S){
       ans = min(ans, right - left + 1); 
       
-      left++;      
+      sum -= arr[left];
+      left++;
     }else{
-      if(right == N) break;
+      if(right == N - 1) break;
 
       right++;
+      sum += arr[right];
     }
+
+    // cout << ans << endl;
+    // cout << left << ' ' << right << ' ' << sum << endl;
   }
 
   cout << ans;
