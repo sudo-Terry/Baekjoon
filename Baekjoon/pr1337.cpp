b628f8d395d1ff66b10d0edad74d73c5bd0e07f4
sudo-Terry <contact.terrykim@gmail.com>
2026-02-07 23:03:45 +0900

    Update pr1337.cpp

--- Baekjoon/pr1337.cpp
+++ Baekjoon/pr1337.cpp
@@ -3,9 +3,6 @@
 #include <algorithm>
 using namespace std;
 
-#define MAX 1000000001
-bool visited[MAX];
-
 int main(){
   ios_base::sync_with_stdio(0);
   cin.tie(nullptr);
@@ -13,26 +10,23 @@ int main(){
 
   int N;
   cin >> N;
-  for(int i = 0; i < N; i++) {
-    int temp;
-    cin >> temp;
-    visited[temp] = true;
+  vector<int> v(N);
+  for(int i = 0; i < N; i++) cin >> v[i];
+
+  sort(v.begin(), v.end());
+
+  int ans = 5;
+  int left = 0;
+
+  for(int right = 0; right < N; right++){
+    while(v[right] - v[left] >= 5){
+      left++;
+    }
+    int cnt = right - left + 1;
+    ans = min(ans, 5 - cnt);
   }
 
-  int cnt = 0; // 윈도우 내에 존재하는 원소 수
-  for(int i = 0; i < 5; i++){
-    if(visited[i]) cnt++;
-  }
-  int ans = 5 - cnt;
-  for(int right = 5; right < MAX; right++){
-    int left = right - 4;
-
-    if(visited[right]) cnt++;
-    if(visited[left - 1]) cnt--;
-  }
-
-  // if(cnt < 5) cout << 5 - cnt;
-  // else cout << '0';
+  cout << ans;
 
   return 0;
 }
