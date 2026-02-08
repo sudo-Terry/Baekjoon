92642b2fb6c127eadb159f6ca3dcf11d13cb6002
sudo-Terry <contact.terrykim@gmail.com>
2026-02-08 15:45:08 +0900

    Update pr7453.cpp

--- Baekjoon/pr7453.cpp
+++ Baekjoon/pr7453.cpp
@@ -6,26 +6,18 @@ using namespace std;
 
 int N;
 
-void push_vector(vector<int>& vec){
-  for(int i = 0; i < N; i++){
-    int temp;
-    cin >> temp;
-    vec.push_back(temp);
-  }
-}
-
 int main(){
   ios_base::sync_with_stdio(0);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  vector<int> A, B, C, D;
-  vector<int> AB, CD;
   cin >> N;
-  push_vector(A);
-  push_vector(B);
-  push_vector(C);
-  push_vector(D);
+  vector<int> AB, CD;
+  vector<int> A(N), B(N), C(N), D(N);
+
+  for(int i = 0; i < N; i++){
+    cin >> A[i] >> B[i] >> C[i] >> D[i];
+  }
 
   for(int i = 0; i < N; i++){
     for(int j = 0; j < N; j++){
@@ -38,7 +30,7 @@ int main(){
       CD.push_back(C[i] + D[j]);
     }
   }
-  sort(CD.begin(), CD.end(), [](int& a, int& b){
+  sort(CD.begin(), CD.end(), [](const int& a, const int& b){
     return a > b;
   });
 
@@ -52,22 +44,22 @@ int main(){
     long long sum = AB[i] + CD[j];
 
     if (sum == 0) {
-      int abVal = AB[i];
-      int cdVal = CD[j];
+      int ab_val = AB[i];
+      int cd_val = CD[j];
 
-      long long abCnt = 0;
-      long long cdCnt = 0;
+      long long ab_cnt = 0;
+      long long cd_cnt = 0;
 
-      while (i < AB.size() && AB[i] == abVal) {
-        abCnt++;
+      while (i < AB.size() && AB[i] == ab_val) {
+        ab_cnt++;
         i++;
       }
-      while (j < CD.size() && CD[j] == cdVal) {
-        cdCnt++;
+      while (j < CD.size() && CD[j] == cd_val) {
+        cd_cnt++;
         j++;
       }
 
-      ans += abCnt * cdCnt;
+      ans += ab_cnt * cd_cnt;
     }
     else if (sum < 0) i++;
     else j++;
