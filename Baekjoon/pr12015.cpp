76e049d76b8aa032253227c01606f5d07e2c1f55
sudo-Terry <contact.terrykim@gmail.com>
2026-02-16 15:24:04 +0900

    Update pr12015.cpp

--- Baekjoon/pr12015.cpp
+++ Baekjoon/pr12015.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
 
+  int N;
+  cin >> N;
+  vector<int> L;
+
+  for (int i = 0; i < N; i++) {
+    int num;
+    cin >> num;
+
+    if (L.empty() || num > L.back()) {
+      L.push_back(num);
+    }else {
+      auto it = lower_bound(L.begin(), L.end(), num);
+      *it = num;
+    }
+  }
+
+  cout << L.size() << '\n';
+
+  return 0;
 }
