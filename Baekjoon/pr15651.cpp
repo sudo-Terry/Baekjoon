932c960dba001369480c855664092aa87fac35a7
sudo-Terry <contact.terrykim@gmail.com>
2026-02-21 17:03:15 +0900

    solve: bj15651

---
+++ Baekjoon/pr15651.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M;
+vector<int> arr;
+
+void dfs() {
+  if (arr.size() == M) {
+    for (int x : arr) {
+      cout << x << ' ';
+    }
+    cout << '\n';
+    return;
+  }
+
+  for (int i = 0; i <= N; i++) {
+    arr.push_back(i);
+    dfs();
+    arr.pop_back();
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> N >> M;
+  dfs();
+  return 0;
+}
