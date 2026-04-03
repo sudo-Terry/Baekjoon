a5e14f3fecee59243b25c7beda8a5888eb5b75ff
sudo-Terry <contact.terrykim@gmail.com>
2026-04-04 02:35:25 +0900

    solve: bj15656

---
+++ Baekjoon/pr15656.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+vector<int> vec;
+vector<int> vec_ans;
+
+void backtrack(){
+  if(vec_ans.size() == M){
+    for(int i = 0; i < M; i++) cout << vec_ans[i] << ' ';
+    cout << '\n';
+    return;
+  }
+
+  int prev = -1;
+  for(int i = 0; i < N; i++){
+    if(vec[i] == prev) continue;
+
+    vec_ans.push_back(vec[i]);
+    backtrack();
+    vec_ans.pop_back();
+
+    prev = vec[i];
+  }
+}
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  cin >> N >> M;
+  int temp;
+  for(int i = 0; i < N; i++){
+    cin >> temp;
+    vec.push_back(temp);
+  }
+  sort(vec.begin(), vec.end());
+
+  backtrack();
+
+  return 0;
+}
