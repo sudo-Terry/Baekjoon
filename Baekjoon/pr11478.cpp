04d8e1720e3b857766b3f0e8b48ef944533755a6
sudo-Terry <contact.terrykim@gmail.com>
2026-04-04 18:48:06 +0900

    solve: bj11478

--- Baekjoon/pr11478.cpp
+++ Baekjoon/pr11478.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <algorithm>
+#include <string>
+#include <set>
 using namespace std;
 
 int main(){
@@ -7,7 +8,18 @@ int main(){
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  
+  string S;
+  cin >> S;
+  set<string> str_set;
+
+for(int i = 0; i < S.size(); i++){
+  for(int len = 1; len <= S.size() - i; len++){
+    string sub_S = S.substr(i, len);
+    str_set.insert(sub_S);
+  }
+}
+
+  cout << str_set.size();
 
   return 0;
 }
